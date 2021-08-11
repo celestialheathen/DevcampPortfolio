89ef6480bb51266eb31bce976c7889e399358c04
celestialheathen <jingchaoyu@hotmail.com>
2021-08-11 15:09:43 -0400

    added virtual attrs

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,14 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern 
+
+included do 
+  before_action :config_params, if: :devise_controller?
+end 
+
+def config_params 
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+end 
+
+
+end 
