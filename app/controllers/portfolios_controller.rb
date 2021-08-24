e43dfd74f86a1efd8fa1b69460f3c97a39b599f7
celestialheathen <jingchaoyu@hotmail.com>
2021-08-24 14:14:23 -0400

    refactored

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
+
   def index 
     @portfolio_items = Portfolio.all
   end 
@@ -26,12 +28,9 @@ class PortfoliosController < ApplicationController
   end 
 
   def edit 
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "The record was successfully updated." }
@@ -42,11 +41,9 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end 
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
 
     respond_to do |format|
@@ -58,6 +55,10 @@ class PortfoliosController < ApplicationController
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+  end 
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end 
 
 end
